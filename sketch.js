7fd4349468e6abec8e0709a324a200b560a926ab
Hako0823 <113077840+Hako0823@users.noreply.github.com>
2023-03-06 21:51:49 -0600

    Add files via upload
    new sketch

--- sketch.js
+++ sketch.js
@@ -1,56 +1,47 @@
-//Este proceso (que ahorita anda comentado) es para guardar una imagen
-let img;
+
 let num1 = 750;
 
-function preload() {
-  img = loadImage('assets/fotoUno.png');
-}
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-    background(255, 0, 200);
+ 
   
 
-  frameRate(15)
-  image(img,-100, 0);
-  image(img,0, 0);
-  tint(100, 0, 40, 126)
-  image(img,70, 0);
-  tint(100, 0, 23, 126)
-  image(img,-70, 0);
-    image(img,0, -60);
+
 }
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
 function draw(){
+  background(6);
   textSize(25)
   fill ( 234, 86, 31);
   textStyle(ITALIC);
-  text("Ayuda",random (num1), random(num1));
-  fill ( 255, 0, 0 );
+   fill ( random(255), random(255),random(255))
+  
+  text("Help",random (num1), random(num1));
+
+   text("Aiuto",random (num1), random(num1));
+
+   text("помогать",random (num1), random(num1));
+  
+   text("Hilfe",random (num1), random(num1));
+  
+   text("Aide",random (num1), random(num1));
+
    text("Ayuda",random (num1), random(num1));
-  fill (  209, 255, 0 );
-   text("Ayuda",random (num1), random(num1));
-     fill ( 249, 28, 95);
-   text("Ayuda",random (num1), random(num1));
-  fill ( 232, 19, 232 );
-   text("Ayuda",random (num1), random(num1));
- fill (  209, 255, 0 );
-   text("Ayuda",random (num1), random(num1));
-    textSize (100)
-  fill (0)
-  text("Ayuda", 250, 500);
+    
+  textSize (100)
+     fill (255)
+  text("Estoy Bien", 100, 400);
+  
  
   if (mouseIsPressed) { 
-    num1 = 1;
+    num1 = 750;
     
   }else {
     num1 = 750
   }
 }
-
-
-
